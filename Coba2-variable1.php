fd0fe8e042a9fecfc81d7ba2d458310798100e25
nyakgym <nyakgymnastiarsyah@gmail.com>
2022-09-22 08:03:55 +0700

    Coding yang dijadikan laporan

---
+++ Coba2-variable1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title> contoh </title>
+</head>
+
+</html>
+
+<body>
+    <?php
+    $nama = "Nyak Gymnastiar Syah";
+    echo "$nama berkata \"Katakan namamu!\"";
+    ?>
+</body>
+
+</html>
